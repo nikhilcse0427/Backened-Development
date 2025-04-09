7cd37513fd09f17fcd60694279d0ddd3650728d6
nikhilcse0427 <nikhilcse0427@gmail.com>
2025-04-09 23:38:03 +0530

    first commit

---
+++ Errors-and-Debugging/syntaxError.js
@@ -0,0 +1,5 @@
+// const var;
+let t1;
+// const t2; -> syntax error because we use const when we are assigning value to variable and we are saying dont change its value
+var t2;
+//gramatical error like missing bracket, ""
