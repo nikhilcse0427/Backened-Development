f5df0c58342f7d9aaf67a0c5fc8d4f81d5f3fd59
nikhilcse0427 <nikhilcse0427@gmail.com>
2025-04-12 13:26:22 +0530

    mongodb connection done

---
+++ BackenedConcept/src/db/db.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+import { Database_Name } from '../constants.js'
+
+const connectDB = async ()=>{
+  try{
+    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${Database_Name}`)
+    console.log("MONGODB CONNECTED !!")
+  }catch(error){
+    console.log("mongodb connection error: ", error)
+    process.exit(1)
+    //process.exit(1) will stop the ENTIRE Node.js application, not just a part of it.
+  }
+}
+
+export default connectDB
